cc40f2947f213dc44f6ea37045b0ddd0b2d22ff4
Nedelin Petkov <nedelin.petkov@delta.bg>
2020-12-10 15:31:01 +0200

    Update smartctl: Add name in returned list

--- plugins/module_utils/facts/extended/smartctl.py
+++ plugins/module_utils/facts/extended/smartctl.py
@@ -43,6 +43,7 @@ class SmartctlFactCollector(BaseFactCollector):
                     'device'         : device[0],
                     'type'           : device[1],
                     'comment'        : device[2],
+                    'name'           : device[1].replace('+', '_').replace(',', '_'),
                     'check_smart.pl' : 'check_smart.pl -d {} -i {}'.format(device[0], device[1])
                 })
             # If have no devices
